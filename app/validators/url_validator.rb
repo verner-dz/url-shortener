a61e5eda355cdfdf6d282d21416acb16e7119387
verner-dz <verner.dz@gmail.com>
2018-04-26 19:11:13 -0400

    add url regex validator

--- app/validators/url_validator.rb
+++ app/validators/url_validator.rb
@@ -3,7 +3,7 @@ class UrlValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     begin
       uri = URI.parse(value)
-      valid_uri = uri.is_a?(URI::HTTP) && !uri.host.nil?
+      valid_uri = valid_url_format?(value) && !uri.host.nil?
     rescue URI::InvalidURIError
       valid_uri = false
     end
@@ -11,5 +11,10 @@ class UrlValidator < ActiveModel::EachValidator
     unless valid_uri
       record.errors[attribute] << (options[:message] || "is not an url")
     end
+  end
+
+  def valid_url_format?(url)
+    url_regex = /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,63}(:[0-9]{1,5})?(\/.*)?\z/ix
+    url_regex.match?(url)
   end
 end
